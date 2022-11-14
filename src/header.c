e866908e54229a49dca7f50ca937cf57012e4d84
MichelH4cker <michel.hecker@usp.br>
2022-11-14 15:29:16 -0300

    add readHeader and printHeader functions

--- src/header.c
+++ src/header.c
@@ -1,1 +1,25 @@
 #include "header.h"
+
+header readHeader(FILE *fp){
+    header header;
+
+    fseek(fp, 0, SEEK_SET);
+    fread(&header.status, sizeof(header.status), 1, fp);
+    fread(&header.noRaiz, sizeof(header.noRaiz), 1, fp);
+    fread(&header.nroChavesTotal, sizeof(header.nroChavesTotal), 1, fp);
+    fread(&header.alturaArvore, sizeof(header.alturaArvore), 1, fp);
+    fread(&header.RRNproxNo, sizeof(header.RRNproxNo), 1, fp);
+
+    return header;
+}
+
+void printHeader(header header){
+    printf("========================\n");
+    printf("        CABEÇALHO       \n");
+    printf("- status: %c\n", header.status);
+    printf("- noRaiz: %d\n", header.noRaiz);
+    printf("- nroChavesTotal: %d\n", header.nroChavesTotal);
+    printf("- alturaArvore: %d\n", header.alturaArvore);
+    printf("- RRNproxNo: %d\n", header.RRNproxNo);
+    printf("========================\n");
+}
