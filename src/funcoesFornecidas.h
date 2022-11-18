9317dd584de8ee88b3cf536cc50ac8fdc69017a9
MichelH4cker <michel.hecker@usp.br>
2022-11-18 14:41:50 -0300

    add funcoesFornecidas

--- src/funcoesFornecidas.h
+++ src/funcoesFornecidas.h
@@ -1,6 +1,12 @@
 #ifndef __FUNCOESFORNECIDAS_H__
 #define __FUNCOESFORNECIDAS_H__
 
+#include "main.h"
 
+void readline(char *string);
+
+void binarioNaTela(char *nomeArquivoBinario);
+
+void scan_quote_string(char *str);
 
 #endif
